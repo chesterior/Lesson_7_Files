f4f7c082af7b273889d2c70fbb31c63744d4cb6d
asafronov <chester1.1@mail.ru>
2022-01-30 15:03:44 +0300

    1

---
+++ src/test/java/guru/qa/FileParsingTest.java
@@ -0,0 +1,69 @@
+package guru.qa;
+
+import com.codeborne.pdftest.PDF;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVReader;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FileParsingTest {
+
+    private ClassLoader cl = FileParsingTest.class.getClassLoader();
+
+    @Test
+    void parsePdfTest() throws Exception {
+        Selenide.open("https://junit.org/junit5/docs/current/user-guide/");
+        File pdfDownload = Selenide.$(byText("PDF download")).download();
+        PDF parsed = new PDF(pdfDownload);
+        assertThat(parsed.author).contains("Marc Philipp");
+    }
+
+//    @Test
+//    void parseXlsTest() throws Exception {
+//        try (InputStream stream = cl.getResourceAsStream("files/sample-xlsx-file.xls")) {
+//            XLS parsed = new XLS(stream);
+//            assertThat(parsed.excel.getSheetAt(0).getRow(0).getCell(0).getStringCellValue())
+//                    .isEqualTo("БЮДЖЕТ-СЕМЬИ.РФ");
+//        }
+//    }
+
+    @Test
+    void parseCsvFile() throws Exception {
+        try (InputStream stream = cl.getResourceAsStream("files/example.csv")) {
+            CSVReader reader = new CSVReader(new InputStreamReader(stream));
+            List<String[]> list = reader.readAll();
+            assertThat(list)
+                    .hasSize(3)
+                    .contains(
+                            new String[] {"Author", "Book"},
+                            new String[] {"Block", "Apteka"},
+                            new String[] {"Esenin", "Cherniy Chelovek"}
+                    );
+        }
+    }
+
+    @Test
+    void zipTest() throws Exception {
+        try (InputStream stream = cl.getResourceAsStream("files/example.csv");
+                ZipInputStream zis = new ZipInputStream(stream)) {
+            ZipEntry zipEntry;
+            while ((zipEntry = zis.getNextEntry()) != null) {
+                assertThat(zipEntry.getName()).isEqualTo("");
+            }
+
+        }
+    }
+}
